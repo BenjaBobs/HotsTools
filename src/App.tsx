82d53d55f4d8544232dbbf4c6d96a5d1d5a6846f
Benjamin <behub92@gmail.com>
2020-08-31 21:56:52 +0200

    Fix dependencies and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import { Result } from 'antd';
-import './App.css';
+import { Layout, Row, Col, Space } from 'antd';
+import style from './App.module.css';
 import { useRecoilValueLoadable } from 'recoil';
 import { s_Heroes } from './api/state/heroes';
+import { Link } from 'react-router-dom';
 
-function App() {
+const { Header, Content } = Layout;
+
+export default function App() {
   const heroData = useRecoilValueLoadable(s_Heroes);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {heroData.state !== 'hasValue' ? (
-          <Result status="info" title="Loading..." />
-        ) : (
-          heroData.contents.map((h) => (
-            <div
-              style={{ width: 128, height: 128 }}
-              onClick={() => alert(h.name)}
-            >
-              <img
-                alt=""
-                src={`https://heroespatchnotes.github.io/heroes-talents/images/heroes/${h.shortName}.png`}
-              />
-            </div>
-          ))
-        )}
-      </header>
-    </div>
+    <Layout className={style.layout}>
+      <Header className={style.header}>
+        <Row align="middle" justify="space-around" className={style.header}>
+          <Col flex={1}>
+            <Space direction="horizontal">
+              <Link to="/">Overview</Link>
+              <Link to="/">Overview2</Link>
+            </Space>
+          </Col>
+        </Row>
+      </Header>
+      <Content className={style.content}>
+        <Row justify="center">
+          <Col style={{ color: 'white' }} flex="1">
+            wat
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import { RecoilRoot } from 'recoil'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { RecoilRoot } from 'recoil';
+import { Router } from 'react-router-dom';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <Router history={history}>
+        <App />
+      </Router>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
