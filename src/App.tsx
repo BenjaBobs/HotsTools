5f9032be140f22e6e81fa72176a674c7e932d27e
Benjamin <behub92@gmail.com>
2020-08-31 18:21:55 +0200

    Test deployment workflow

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is a test of deployment
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Does deployment work?
         </a>
       </header>
     </div>
