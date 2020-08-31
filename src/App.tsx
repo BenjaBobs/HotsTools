2534cf9b275b506945227b5cf40f7bb9f4aa6c56
Benjamin <behub92@gmail.com>
2020-08-31 19:38:43 +0200

    Load hero data

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Result } from 'antd';
 import './App.css';
+import { useRecoilValueLoadable } from 'recoil';
+import { s_Heroes } from './api/state/heroes';
 
 function App() {
+  const heroData = useRecoilValueLoadable(s_Heroes);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Result status="404" />
-        <p>
-          This is a test of deployment
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Does deployment work?
-        </a>
+        {heroData.state !== 'hasValue' ? (
+          <Result status="info" title="Loading..." />
+        ) : (
+          heroData.contents.map((h) => (
+            <div
+              style={{ width: 128, height: 128 }}
+              onClick={() => alert(h.name)}
+            >
+              <img
+                alt=""
+                src={`https://heroespatchnotes.github.io/heroes-talents/images/heroes/${h.shortName}.png`}
+              />
+            </div>
+          ))
+        )}
       </header>
     </div>
   );
