9ce624414d236902e6dab39d5676cb093afb0419
Benjamin <behub92@gmail.com>
2020-08-31 18:43:51 +0200

    Test AntD in deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Result } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Result status="404" />
         <p>
           This is a test of deployment
         </p>
