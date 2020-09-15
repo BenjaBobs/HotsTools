1ed6f20a8a94b2e58ef4a0292c5fd84f4ec515b9
Benjamin <behub92@gmail.com>
2020-09-15 22:54:57 +0200

    Wip hero picking

--- src/apps/draft-simulator/draft-simulator-app.tsx
+++ src/apps/draft-simulator/draft-simulator-app.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppDefinition } from '../AppDefinition';
-import { Row, Col, Steps } from 'antd';
+import { Row, Col, Steps, Avatar } from 'antd';
 import HeroSelectionColumn from './components/hero-selection-column';
+import { useRecoilValue } from 'recoil';
+import { s_Heroes } from '../../api/state/heroes';
+import { GetHeroIcon } from '../../api/HotsTalents';
+import { Hero } from '../../api/state/hero-types';
 
 const DraftSimulatorApp: AppDefinition = {
   name: 'Draft simulator',
@@ -32,6 +36,11 @@ const RedDot = (
 );
 
 function DraftSimulator() {
+  const heroData = useRecoilValue(s_Heroes);
+  const [blueHeroes, setBlueHeroes] = useState<Hero[]>([]);
+  const [redHeroes, setRedHeroes] = useState<Hero[]>([]);
+  const [phase, setPhase] = useState<'bluePick' | 'redPick'>('bluePick');
+
   return (
     <>
       <Row justify="center">
@@ -46,23 +55,44 @@ function DraftSimulator() {
             <Steps.Step title="Pick" icon={RedDot} />
             <Steps.Step title="Pick" icon={BlueDot} />
             <Steps.Step title="Pick" icon={RedDot} />
-            <Steps.Step title="Pick" />
-            <Steps.Step title="Pick" />
-            <Steps.Step title="Pick" />
-            <Steps.Step title="Pick" />
-            <Steps.Step title="Pick" />
+            <Steps.Step title="Ban" icon={RedDot} />
+            <Steps.Step title="Ban" icon={RedDot} />
+            <Steps.Step title="Pick" icon={RedDot} />
+            <Steps.Step title="Pick" icon={RedDot} />
+            <Steps.Step title="Pick" icon={RedDot} />
           </Steps>
         </Col>
       </Row>
       <Row justify="space-between" align="middle">
-        <Col style={{ height: 500, width: 200 }}>
-          <HeroSelectionColumn selectedHeroes={[]} />
+        <Col span={3} style={{ height: 500 }}>
+          <HeroSelectionColumn selectedHeroes={blueHeroes.map((h) => h.icon)} />
         </Col>
-        <Col>hero searcher</Col>
-        <Col style={{ height: 500, width: 200 }}>
-          <HeroSelectionColumn
-            selectedHeroes={['abathur.png', 'brightwing.png']}
-          />
+        <Col span={18}>
+          {heroData
+            .filter((h) => !blueHeroes.includes(h) && !redHeroes.includes(h))
+            .map((hero) => (
+              <span
+                key={hero.id}
+                onClick={() => {
+                  if (phase == 'bluePick') {
+                    setBlueHeroes([...blueHeroes, hero]);
+                  } else {
+                    setRedHeroes([...redHeroes, hero]);
+                  }
+
+                  setPhase(phase === 'bluePick' ? 'redPick' : 'bluePick');
+                }}
+              >
+                <Avatar
+                  className="outline hoverable"
+                  style={{ height: 80, width: 80 }}
+                  src={GetHeroIcon(hero.icon)}
+                />
+              </span>
+            ))}
+        </Col>
+        <Col span={3} style={{ height: 500 }}>
+          <HeroSelectionColumn selectedHeroes={redHeroes.map((h) => h.icon)} />
         </Col>
       </Row>
     </>
