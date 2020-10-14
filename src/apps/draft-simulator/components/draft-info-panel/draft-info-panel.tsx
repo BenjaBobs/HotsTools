2c939dd763fc2b138b4d39f3d165ba759f5ca784
Benjamin <behub92@gmail.com>
2020-10-14 22:58:21 +0200

    Switched nivo for recharts and wip on team strength charts

--- src/apps/draft-simulator/components/draft-info-panel/draft-info-panel.tsx
+++ src/apps/draft-simulator/components/draft-info-panel/draft-info-panel.tsx
@@ -1,5 +1,12 @@
-import { Card } from 'antd';
-import React from 'react';
+import { Card, Row, Col } from 'antd';
+import React, { useMemo } from 'react';
+import {
+  PolarAngleAxis,
+  PolarGrid,
+  Radar,
+  RadarChart,
+  ResponsiveContainer,
+} from 'recharts';
 import { Phase, PhaseActions } from '../../draft-simulator-app';
 import './draft-info-panel.scss';
 
@@ -7,9 +14,61 @@ export default function DraftInfoPanel(props: {
   phase: Phase;
   history: PhaseActions[];
 }) {
+  const blueHeroes = props.history
+    .filter((x) => x.team === 'blue' && x.type === 'Pick')
+    .flatMap((x) => x.heroes);
+
+  const data = useMemo(() => {
+    if (!blueHeroes.length) return [];
+
+    const accumulator: { [key: string]: any } = {};
+
+    for (const category of Object.keys(blueHeroes[0].extensions.strengths)) {
+      accumulator[category] = { category: category, sum: 0 };
+
+      for (const hero of blueHeroes) {
+        accumulator[category][hero.shortName] =
+          (hero.extensions.strengths as any)[category] +
+          accumulator[category].sum;
+        accumulator[category].sum = accumulator[category][hero.shortName];
+      }
+    }
+
+    return Object.values(accumulator);
+  }, [blueHeroes]);
+
   return (
     <Card className={'draft-info-panel'} title="Info">
-      This is where the crucial info about what to Pick next will show
+      <Row justify="space-between">
+        <Col flex="1" style={{ height: 300 }}>
+          {blueHeroes.length ? (
+            <ResponsiveContainer>
+              <RadarChart
+                className="team-strength-chart"
+                outerRadius={100}
+                data={data}
+              >
+                <PolarGrid />
+                <PolarAngleAxis dataKey="category" />
+                {blueHeroes.length &&
+                  blueHeroes?.map((hero) => (
+                    <Radar
+                      key={hero.shortName}
+                      name=""
+                      dataKey={hero.shortName}
+                      stroke="#aaaaff"
+                      fill="#aaaaff"
+                      fillOpacity={0.2}
+                    />
+                  ))}
+              </RadarChart>
+            </ResponsiveContainer>
+          ) : (
+            'blue team chart'
+          )}
+        </Col>
+        <Col flex="1">red</Col>
+      </Row>
     </Card>
   );
 }
--- src/apps/overview/pages/components/hero-strengths-chart.tsx
+++ src/apps/overview/pages/components/hero-strengths-chart.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import { ResponsiveRadar } from '@nivo/radar';
 import { Hero } from '../../../../api/state/hero-types';
+import {
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  Radar,
+  ResponsiveContainer,
+} from 'recharts';
 
 export default function HeroStrengthsChart(props: { hero: Hero }) {
-  const keys = [props.hero.name];
-
   const data = [
     ...Object.entries(props.hero.extensions.strengths).entries(),
   ].map(([index, [category, value]]) => {
-    let datum: any = {};
-
-    datum.category = category;
-    datum[props.hero.name] = value;
-
-    return datum;
+    return {
+      category,
+      value,
+    };
   });
 
   return (
-    <ResponsiveRadar
-      maxValue={10}
-      gridLevels={5}
-      gridShape="linear"
-      data={data}
-      indexBy="category"
-      keys={keys}
-      colors="red"
-      margin={{ left: 200, right: 200, top: 128, bottom: 128 }}
-      enableDotLabel={true}
-      dotLabel="value"
-      dotLabelYOffset={-12}
-      fillOpacity={0.2}
-      isInteractive={false}
-      legends={[]}
-    />
+    <ResponsiveContainer>
+      <RadarChart outerRadius={90} data={data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="category" />
+        <Radar
+          name=""
+          dot
+          dataKey="value"
+          stroke="#ff0000"
+          fill="#ff0000"
+          fillOpacity={0.6}
+        />
+      </RadarChart>
+    </ResponsiveContainer>
   );
 }
